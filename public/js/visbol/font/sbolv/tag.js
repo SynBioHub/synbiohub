4e32a28c9b50d884eb4b3ce6186f38f48e95cbf5
James Alastair McLaughlin <james@clickteam.com>
2017-01-30 13:51:53 +0000

    Add tag glyph (needs some work)

--- public/js/visbol/font/sbolv/tag.js
+++ public/js/visbol/font/sbolv/tag.js
@@ -5,23 +5,58 @@ define([ 'visbol' ], function(visbol) {
         Rect = visbol.Rect,
         Matrix = visbol.Matrix;
 
+    function createGeometry(boxSize) {
+
+        var headWidth = boxSize.y / 2.0;
+
+        return {
+            boxStart: Vec2(0, 0),
+            boxTopRight: Vec2(boxSize.x - headWidth, 0),
+            headPoint: Vec2(boxSize.x, boxSize.y / 2.0),
+            boxBottomRight: Vec2(boxSize.x - headWidth, boxSize.y),
+            boxBottomLeft: Vec2(0, boxSize.y)
+        };
+    }
+
     function renderGlyph(design, glyphObject, boxSize) {
 
-        var glyph = design.surface.rect(boxSize.x, boxSize.y);
+        var geom = createGeometry(boxSize);
+
+        var path = [
+
+            'M' + Vec2.toPathString(geom.boxStart),
+            'L' + Vec2.toPathString(geom.boxTopRight),
+            'L' + Vec2.toPathString(geom.headPoint),
+            'L' + Vec2.toPathString(geom.boxBottomRight),
+            'L' + Vec2.toPathString(geom.boxBottomLeft),
+            'Z'
+
+        ].join('');
+
+        var glyph = design.surface.path(path);
 
         glyph.attr('stroke', 'black');
-        glyph.attr('fill', glyphObject.color || '#cee');
+        glyph.attr('fill', 'pink')
 
         if(glyphObject.uri)
             glyph.attr('data-uri', glyphObject.uri);
 
-        var group = design.surface.group();
+            var glyphMatrix = Matrix();
+            var labelMatrix = Matrix();
 
-        group.add(glyph);
+
+
+        glyph.transform({
+            matrix: Matrix.toSVGString(Matrix.rotation(-135))
+            //matrix: Matrix.toSVGString(Matrix())
+        })
+
+        var group = design.surface.group()
+        group.add(glyph)
 
         return {
             glyph: group,
-            backboneOffset: boxSize.y
+            backboneOffset: boxSize.y / 2.0
         };
     }
 
@@ -29,10 +64,12 @@ define([ 'visbol' ], function(visbol) {
 
         render: renderGlyph,
 
-      insets: {
-            top: 0.5
+        insets: {
+            top: 0.35,
+            bottom: 0.35,
+            left: 0.2,
+            right: 0.2
         }
-
 
     };
 });
