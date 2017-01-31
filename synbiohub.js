efb2538e9fffd1d6f99a2fc878914d0df80891d2
James Alastair McLaughlin <j.a.mclaughlin@ncl.ac.uk>
2017-01-31 19:49:58 +0000

    Part one of merging sbol stack API and synbiohub
    MongoDB has also been removed in favour of sqlite using the sequelize ORM.
    Lots to fix!

--- synbiohub.js
+++ synbiohub.js
@@ -1,10 +1,6 @@
 
 var config = require('./lib/config')
 
-var mongoose = require('mongoose')
-
-mongoose.connect(config.get('databaseHost'), config.get('databaseName'))
-
 var App = require('./lib/app')
 
 var app = new App()
