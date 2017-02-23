d2d2085223182062fec5ce06a4f2aa9e1d3bfc3a
James Alastair McLaughlin <j.a.mclaughlin@ncl.ac.uk>
2017-02-23 13:42:04 +0000

    editable description and notes

--- browser/synbiohub.js
+++ browser/synbiohub.js
@@ -66,7 +66,7 @@ if(document.getElementById('design')
 
 
 
-$('#sbh-add-description').click(function() {
+$(document).on('click', '#sbh-add-description', function() {
 
     var $textarea = $('<textarea class="form-control"></textarea>')
     var $saveButton = $('<button class="btn btn-primary">').text('Save Description')
@@ -88,9 +88,8 @@ $('#sbh-add-description').click(function() {
         $.post('/updateMutableDescription', {
             uri: meta.uri,
             desc: desc,
-            success: function(res) {
-                $div.replaceWith($(res))
-            }
+        }, function(res) {
+            $div.replaceWith($(res))
         })
 
     })
@@ -100,8 +99,102 @@ $('#sbh-add-description').click(function() {
 
 })
 
-$('#sbh-add-notes').click(function() {
+
+$(document).on('click', '#sbh-edit-description', function() {
+
+    var $textarea = $('<textarea class="form-control"></textarea>').val($('#sbh-description').data('src'))
+    var $saveButton = $('<button class="btn btn-primary">').text('Save Description')
+    var $cancelButton = $('<button class="btn btn-default">').text('Cancel')
+
+    var $div = $('<div></div>').append($textarea).append($saveButton).append($cancelButton)
+
+    var $orig = $('#sbh-description')
+    $('#sbh-description').replaceWith($div)
+
+    $cancelButton.click(function() {
+        $div.replaceWith($orig)
+    })
+
+    $saveButton.click(function() {
+
+        var desc = $textarea.val()
+
+        $.post('/updateMutableDescription', {
+            uri: meta.uri,
+            desc: desc,
+        }, function(res) {
+            $div.replaceWith($(res))
+        })
+
+    })
+
+
+    $textarea.focus()
+})
+
+$(document).on('click', '#sbh-add-notes', function() {
+
+    var $textarea = $('<textarea class="form-control"></textarea>')
+    var $saveButton = $('<button class="btn btn-primary">').text('Save Notes')
+    var $cancelButton = $('<button class="btn btn-default">').text('Cancel')
+
+    var $div = $('<div></div>').append($textarea).append($saveButton).append($cancelButton)
+
+    var $orig = $(this)
+    $(this).replaceWith($div)
+
+    $cancelButton.click(function() {
+        $div.replaceWith($orig)
+    })
+
+    $saveButton.click(function() {
+
+        var notes = $textarea.val()
+
+        $.post('/updateMutableNotes', {
+            uri: meta.uri,
+            notes: notes,
+        }, function(res) {
+            $div.replaceWith($(res))
+        })
+
+    })
+
+
+    $textarea.focus()
 
 })
 
+
+$(document).on('click', '#sbh-edit-notes', function() {
+
+    var $textarea = $('<textarea class="form-control"></textarea>').val($('#sbh-notes').data('src'))
+    var $saveButton = $('<button class="btn btn-primary">').text('Save Notes')
+    var $cancelButton = $('<button class="btn btn-default">').text('Cancel')
+
+    var $div = $('<div></div>').append($textarea).append($saveButton).append($cancelButton)
+
+    var $orig = $('#sbh-notes')
+    $('#sbh-notes').replaceWith($div)
+
+    $cancelButton.click(function() {
+        $div.replaceWith($orig)
+    })
+
+    $saveButton.click(function() {
+
+        var notes = $textarea.val()
+
+        $.post('/updateMutableNotes', {
+            uri: meta.uri,
+            notes: notes,
+        }, function(res) {
+            $div.replaceWith($(res))
+        })
+
+    })
+
+
+    $textarea.focus()
+})
 
