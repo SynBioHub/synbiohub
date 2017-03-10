12b3fe13c61117f5f8a19e51f9d5af41544878fd
James Alastair McLaughlin <j.a.mclaughlin@ncl.ac.uk>
2017-03-10 00:03:38 +0000

    Add n3 to rdfxml script

--- scripts/split_to_n3.sh
+++ scripts/split_to_n3.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+#
+# This script splits large rdf+xml files up into multiple n3 triple files.
+#
+# Args: the filename of an rdf+xml file to split
+# Output: a set of n3 triple files named upload_* 
+# 
+
 rapper -o ntriples $@ > ntriples.n3 && split -l 5000 ntriples.n3 upload_
 
 
