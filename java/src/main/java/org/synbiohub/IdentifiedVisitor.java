4395231a0e452e74410430f09b11fd58c38c01d0
Chris Myers <myers@ece.utah.edu>
2017-04-03 13:20:30 +0100

    Add topLevel to visit

--- java/src/main/java/org/synbiohub/IdentifiedVisitor.java
+++ java/src/main/java/org/synbiohub/IdentifiedVisitor.java
@@ -21,48 +21,48 @@ import org.sbolstandard.core2.TopLevel;
 
 public abstract class IdentifiedVisitor
 {
-	public abstract void visit(Identified identified);
+	public abstract void visit(Identified identified, TopLevel topLevel);
 	
 	public void visitDocument(SBOLDocument sbolDocument)
 	{
 		for (TopLevel topLevel : sbolDocument.getTopLevels())
 		{		
-			visit(topLevel);
+			visit(topLevel, topLevel);
 			
 			if (topLevel instanceof ComponentDefinition) {
 				for (Component c : ((ComponentDefinition) topLevel).getComponents()) {
-					visit(c);
+					visit(c, topLevel);
 					for (MapsTo m : c.getMapsTos()) {
-						visit(m);
+						visit(m, topLevel);
 					}
 				}
 				for (SequenceAnnotation sa : ((ComponentDefinition) topLevel).getSequenceAnnotations()) {
-					visit(sa);
+					visit(sa,topLevel);
 					for (Location l : sa.getLocations()) {
-						visit(l);
+						visit(l,topLevel);
 					}
 				}
 				for (SequenceConstraint sc : ((ComponentDefinition) topLevel).getSequenceConstraints()) {
-					visit(sc);
+					visit(sc,topLevel);
 				}
 			}
 			else if (topLevel instanceof ModuleDefinition) {
 				for (FunctionalComponent c : ((ModuleDefinition) topLevel).getFunctionalComponents()) {
-					visit(c);
+					visit(c,topLevel);
 					for (MapsTo m : c.getMapsTos()) {
-						visit(m);
+						visit(m,topLevel);
 					}
 				}
 				for (Module mod : ((ModuleDefinition) topLevel).getModules()) {
-					visit(mod);
+					visit(mod,topLevel);
 					for (MapsTo m : mod.getMapsTos()) {
-						visit(m);
+						visit(m,topLevel);
 					}
 				}
 				for (Interaction i : ((ModuleDefinition) topLevel).getInteractions()) {
-					visit(i);
+					visit(i,topLevel);
 					for (Participation p : i.getParticipations()) {
-						visit(p);
+						visit(p,topLevel);
 					}
 				}
 			}
