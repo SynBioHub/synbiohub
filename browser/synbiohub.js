b79868dd2e47892433cc6ca6b34f4fb68b6e9952
Chris Myers <myers@ece.utah.edu>
2017-05-20 11:47:20 -0400

    Added User Types

--- browser/synbiohub.js
+++ browser/synbiohub.js
@@ -39,7 +39,9 @@ $('.sbh-datatable .save').click(function() {
         name: $(inputs[2]).val(),
         email: $(inputs[3]).val(),
         affiliation: $(inputs[4]).val(),
-        isAdmin: $(inputs[5]).prop('checked')
+        isMember: $(inputs[5]).prop('checked'),
+        isCurator: $(inputs[6]).prop('checked'),
+        isAdmin: $(inputs[7]).prop('checked')
     }
 
     $.post('/admin/updateUser', userInfo, function() {
@@ -61,7 +63,9 @@ $('.sbh-datatable .delete').click(function() {
         name: $(inputs[2]).val(),
         email: $(inputs[3]).val(),
         affiliation: $(inputs[4]).val(),
-        isAdmin: $(inputs[5]).prop('checked')
+        isMember: $(inputs[5]).prop('checked'),
+        isCurator: $(inputs[6]).prop('checked'),
+        isAdmin: $(inputs[7]).prop('checked')
     }
 
     $.post('/admin/deleteUser', userInfo, function() {
