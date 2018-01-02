a03640b9a045a178258124d5c7c5f85fe14d38e2
Zach Zundel <me@zachzundel.com>
2018-01-02 15:45:17 -0700

    Initial Java files

---
+++ java/src/main/java/org/synbiohub/BuildCombineArchiveJob.java
@@ -0,0 +1,12 @@
+package org.synbiohub;
+
+import java.net.URI;
+
+public class BuildCombineArchiveJob extends Job {
+	public URI rootCollectionUri;
+	public String archiveFilename;
+	
+	public void execute() {
+		SynBioHubFrontend synbiohub
+	}
+}
---
+++ java/src/main/java/org/synbiohub/BuildCombineArchiveResult.java
@@ -0,0 +1,7 @@
+package org.synbiohub;
+
+public class BuildCombineArchiveResult extends Result {
+	public BuildCombineArchiveResult(BuildCombineArchiveJob job) {
+		super(job);
+	}
+}
