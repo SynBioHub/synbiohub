3163b8d298361e65c4bbdd3d9fb47eba02275cef
Zach Zundel <me@zachzundel.com>
2018-01-02 22:35:43 -0700

    Begin building COMBINE archive

--- java/src/main/java/org/synbiohub/BuildCombineArchiveResult.java
+++ java/src/main/java/org/synbiohub/BuildCombineArchiveResult.java
@@ -1,7 +1,20 @@
 package org.synbiohub;
 
 public class BuildCombineArchiveResult extends Result {
-	public BuildCombineArchiveResult(BuildCombineArchiveJob job) {
+	private boolean success;
+	private String errorLog;
+	private String resultFilename;
+	
+	public BuildCombineArchiveResult(
+			BuildCombineArchiveJob job,
+			boolean success,
+			String resultFilename,
+			String errorLog)
+	{
 		super(job);
+		
+		this.success = success;
+		this.resultFilename = resultFilename;
+		this.errorLog = errorLog;
 	}
 }
