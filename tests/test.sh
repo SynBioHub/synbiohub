7365ceb8dc7b36fa6c386b3e5c031ed86263ce6a
oliver <oflatt@gmail.com>
2018-12-05 19:36:56 -0700

    add testing containers

--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-echo "Not implemented, so it passed"
+if cd synbiohub-docker; then
+    git pull;
+    cd ..;
+else
+    # clone the synbiohub docker compose file in order to run docker containers
+    git clone https://github.com/synbiohub/synbiohub-docker;
+fi
+
+
+docker-compose -f ./synbiohub-docker/docker-compose.yml up
