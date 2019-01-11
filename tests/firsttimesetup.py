bb56b2ef0f2d2e287a1bf3451fc95d872050c8f8
oliver <oflatt@gmail.com>
2019-01-11 12:14:05 -0700

    working on checking functions

--- tests/firsttimesetup.py
+++ tests/firsttimesetup.py
@@ -1,6 +1,35 @@
-import requests
+import requests, difflib
+
+saveForTesting = True
+
+# request- string, the name of the page being requested
+# todo- test and use, currently a rough outline
+def comparegetrequest(request):
+    if request[0] == '/':
+        request = request[1:]
+    response = requests.get('http://localhost:7777/' + request)
+
+    filepath = 'tests/previousresults/getrequest_' + request + '.txt'
+    if saveForTesting:
+        with open(filepath, 'w') as rfile:
+            rfile.write(response.text)
+    else:
+        olddata = None
+        with open (filepath, "r") as oldfile:
+            olddata=oldfile.readlines
+
+        
+        newdata = request.text.splitlines()
+        
+        
+        changes = difflib.unified_diff(olddata, newdata)
+
+        for c in changes:
+            print(c)
+
 
 setup = {
+    'userName' : 'testuser',
     'userEmail': 'test@user.synbiohub',
     'userPassword': 'test',
     'userPasswordConfirm': 'test',
@@ -14,7 +43,6 @@ setup = {
     'allowPublicSignup': 'true',
 }
 
-print(setup)
 
 response = requests.post('http://localhost:7777/setup', data=setup)
 
