54eb8f04ae4492c9057d783bc76c3a8506a99739
oliver <oflatt@gmail.com>
2019-02-10 15:39:38 -0700

    move start containers into bash script

--- tests/test.sh
+++ tests/test.sh
@@ -12,31 +12,8 @@ then
 fi
 
 message "Running synbiohub test suite."
-message "Cleaning old test containers if they exist"
 
-bash ./testcleanup.sh
-
-
-message "pulling synbiohub/synbiohub-docker"
-if cd synbiohub-docker; then
-    git checkout snapshot;
-    git pull;
-    cd ..;
-else
-    # clone the synbiohub docker compose file in order to run docker containers
-    git clone --single-branch --branch snapshot https://github.com/synbiohub/synbiohub-docker
-fi
-
-
-message "Starting SynBioHub from Containers"
-docker-compose -f ./synbiohub-docker/docker-compose.yml -p testsuiteproject up -d
-while [[ "$(docker inspect testsuiteproject_synbiohub_1 | jq .[0].State.Health.Status)" != "\"healthy\"" ]]
-do
-    sleep 5
-    message "Waiting for synbiohub container to be healthy."
-done
-
-message "Started successfully"
+bash ./start_containers.sh
 
 for var in "$@"
 do
@@ -46,6 +23,7 @@ do
 	exit 1
     fi
 done
+
 
 message "Running test suite."
 
