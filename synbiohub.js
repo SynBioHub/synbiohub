06a567ef0180f6c4e7ee863b39a28425a9f5201f
Zach Zundel <me@zachzundel.com>
2019-03-06 14:21:31 -0700

    Replace console logs with correct loggers

--- synbiohub.js
+++ synbiohub.js
@@ -1,14 +1,18 @@
-var config = require('./lib/config')
-var App = require('./lib/app')
-var db = require('./lib/db')
-var fs = require('fs')
-var jobUtils = require('./lib/jobs/job-utils')
-var sliver = require('./lib/sliver')
-var theme = require('./lib/theme')
-var java = require('./lib/java')
-var gitRev = require('./lib/gitRevision')
+const config = require('./lib/config')
+const App = require('./lib/app')
+const db = require('./lib/db')
+const fs = require('fs')
+const jobUtils = require('./lib/jobs/job-utils')
+const java = require('./lib/java')
+const gitRev = require('./lib/gitRevision')
+const logger = require('./lib/logger')
+const theme = require('./lib/theme')
 
-if (!fs.existsSync('synbiohub.sqlite') || fs.statSync('synbiohub.sqlite').size == 0) {
+console.log = logger.info.bind(logger)
+console.warn = logger.warn.bind(logger)
+console.error = logger.error.bind(logger)
+
+if (!fs.existsSync('synbiohub.sqlite') || fs.statSync('synbiohub.sqlite').size === 0) {
   db.sequelize.sync({ force: true }).then(startServer)
 } else {
   db.umzug.up().then(() => {
