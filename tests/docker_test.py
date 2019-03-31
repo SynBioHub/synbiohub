c962ac03ed9ee8022a3c36062b396a98ec749350
oliver <oflatt@gmail.com>
2019-03-31 11:49:15 -0600

    add basic persistance test

---
+++ tests/docker_test.py
@@ -0,0 +1,44 @@
+import requests
+from unittest import TestCase
+from test_functions import compare_get_request, compare_post_request, login_with
+
+
+class TestDocker(TestCase):
+
+    def test_dockeruser_persist(self):
+
+        # login as the user
+        logininfo = {'email' : 'dockertestuser',
+                     'password' : 'test'}
+        login_with(logininfo)
+
+        # test the project view
+        compare_get_request("manage", test_name="docker")
+        compare_get_request("/user/:userId/:collectionId/:displayId",
+                            route_parameters = ["dockertestuser","testid_sequence1","testid_sequence1_collection"], test_name="docker")
+
+
+    def test_public_persist(self):
+        # login as the user
+        logininfo = {'email' : 'dockertestuser',
+                     'password' : 'test'}
+        login_with(logininfo)
+        
+        # test public ones while signed in as user
+        compare_get_request("public/:collectionId/:displayId",
+                            route_parameters = ["testid", "testid_collection"],
+                            test_name = "docker_public")
+        compare_get_request("public/:collectionId/:displayId(*)/:version/full",
+                            route_parameters = ["testid", "testid_collection", "1"])
+
+        # test the component page
+        compare_get_request("public/:collectionId/:displayId",
+                            route_parameters = ["testid", "BBa_I0462"], test_name="bbapublic")
+
+    def test_admin_persist(self):
+        logininfo = {'email' : 'test@user.synbiohub',
+                     'password' : 'test'}
+        login_with(logininfo)
+        compare_get_request("manage", test_name="docker_admin")
+        
+
