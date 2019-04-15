1b52e37eb9a9186bfefc06cab9f1ff040f109e6c
Chris Myers <myers@ece.utah.edu>
2019-04-15 12:00:34 -0700

    Update admin test

--- tests/test_admin_views.py
+++ tests/test_admin_views.py
@@ -32,6 +32,9 @@ class TestAdminViews(TestCase):
     def test_admin_theme(self):
         compare_get_request("/admin/theme")
 
+    def test_admin_theme(self):
+        compare_get_request("/admin/sparql")
+
     # TODO: fix backup in docker containers
     #def test_admin_backup(self):
     #    compare_get_request("admin/backup")
