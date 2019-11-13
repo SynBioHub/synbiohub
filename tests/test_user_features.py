80fc66b5486d7922c9ca0b2406648e79b2b1b6f7
jvscholz <jvscholz@gmail.com>
2019-11-13 06:52:46 -0700

    tests endpoint user/part/createTest

--- tests/test_user_features.py
+++ tests/test_user_features.py
@@ -5,3 +5,6 @@ from test_functions import compare_get_request, compare_post_request
 class TestFeatures(TestCase):
         def test_user_part_addOwner(self):
                 compare_get_request("/user/:userId/:collectionId/:displayId/:version/addOwner", route_parameters = ["testuser","testid0","BBa_I0462", "1"])
+
+        def test_user_part_createTest(self):
+                compare_get_request("/user/:userId/:collectionId/:displayId/:version/createTest", route_parameters = ["testuser","testid0","BBa_I0462", "1"])
