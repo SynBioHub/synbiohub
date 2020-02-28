31ae25dc947794068b20ec8eff9bf4f22e659986
Zach Zundel <me@zachzundel.com>
2020-02-27 18:42:04 -0700

    Fix tests

--- migrations/007-rename-alias-uri-to-url.js
+++
@@ -1,9 +0,0 @@
-// Migration to change uri column to url in Alias table
-module.exports = {
-  up: function (query, DataTypes) {
-    return query.renameColumn('Alias', 'uri', 'url')
-  },
-  down: (query, DataTypes) => {
-    return query.renameColumn('Alias', 'url', 'uri')
-  }
-}
