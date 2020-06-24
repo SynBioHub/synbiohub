9188448865ca5796b40df6d92e0c34e6992640dd
jvscholz <jvscholz@gmail.com>
2020-06-24 11:24:43 -0600

    Revert "adds test post for profile"
    This reverts commit df620d48bec280f257da4b61b4e0d3d14b7b2e53.

--- tests/test_user.py
+++ tests/test_user.py
@@ -10,7 +10,6 @@ class TestUser(TestCase):
                      'password' : 'test'}
         login_with(logininfo)
 
-    # GET <SynBioHub URL>/logout
     def test_logout(self):
         compare_get_request("/logout")
 
@@ -19,18 +18,7 @@ class TestUser(TestCase):
                      'password' : 'test'}
         login_with(logininfo)
 
-    # GET <SynBioHub URL>/profile
     def test_profile(TestCase):
         compare_get_request("/profile")
 
-    # POST <SynBioHub URL>/profile
-    def test_post_profile(self):
-        data={
-            'name': 'Jimmy',
-            'affiliation' : 'NerdsInc',
-            'email' : '',
-            'password1' : '<password1>',
-            'password2' : '<password2>'}
-        compare_post_request("profile", data, headers = {"Accept":\
-            "text/plain"}, test_name = "test_update_profile")
 
