bc9e925395d55148fae926575d4ad559efcbb657
jvscholz <jvscholz@gmail.com>
2020-07-06 07:59:18 -0600

    currenly broken

--- tests/test_attachment.py
+++ tests/test_attachment.py
@@ -5,13 +5,13 @@ from test_functions import compare_get_request, compare_post_request
 
 class TestAttachment(TestCase):
 
-#    def test_attach(self):
-#        files={
-#            'file' : open('test_user.py', 'rb')
-#        }
-#        data = {
-#        }
-#        compare_post_request("/public/:collectionId/:displayId/:version/attach", data, route_parameters = ["testid0", "testid_collection0", "1"],headers = {"Accept": "text/plain"}, files = files, test_name = "test_attach")
+    def test_attach(self):
+        files={
+            'file' : open('test_user.py', 'rb')
+        }
+        data = {
+        }
+        compare_post_request("/public/:collectionId/:displayId/:version/attach", data, route_parameters = ["testid0", "testid_collection0", "1"],headers = {"Accept": "text/plain"}, files = files, test_name = "test_attach")
 
     def test_attachUrl(self):
         #http://localhost:7777/public/testid0/testid0_collection/1/attachUrl
