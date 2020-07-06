1890635b1b022ccd50bf71d7965e48e1f82eed38
jvscholz <jvscholz@gmail.com>
2020-07-06 07:51:13 -0600

    tests attach url

--- tests/test_attachment.py
+++ tests/test_attachment.py
@@ -5,11 +5,19 @@ from test_functions import compare_get_request, compare_post_request
 
 class TestAttachment(TestCase):
 
-    def test_attach(self):
-        image_filename = os.path.basename('./logo.jpg');
-        files={
-            'file' : (image_filename, open('./logo.jpg', 'rb')),
-        }
+#    def test_attach(self):
+#        files={
+#            'file' : open('test_user.py', 'rb')
+#        }
+#        data = {
+#        }
+#        compare_post_request("/public/:collectionId/:displayId/:version/attach", data, route_parameters = ["testid0", "testid_collection0", "1"],headers = {"Accept": "text/plain"}, files = files, test_name = "test_attach")
+
+    def test_attachUrl(self):
+        #http://localhost:7777/public/testid0/testid0_collection/1/attachUrl
         data = {
+            'url': 'testsite.com',
+            'name' : 'testURLAttachment',
+            'type' : 'dim'
         }
-        compare_post_request("/public/:collectionId/:displayId/:version/attach", data, route_parameters = ["testid0", "testid0", "testid_collection0", "1"],headers = {"Accept": "text/plain"}, test_name = "test_attach")
+        compare_post_request("/public/:collectionId/:displayId/:version/attachUrl", data, route_parameters = ["testid0", "testid_collection0", "1"],headers = {"Accept": "text/plain"}, test_name = "test_attachUrl")
