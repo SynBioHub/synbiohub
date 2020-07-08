300e78c3c245c022396a36db7835795d5967a84f
jvscholz <jvscholz@gmail.com>
2020-07-08 07:38:55 -0600

    tests more edit endponts

--- tests/test_edit.py
+++ tests/test_edit.py
@@ -7,6 +7,27 @@ class TestEdit(TestCase):
     def test_edit_field(self):
         data={
             'uri': 'http://localhost:7777/public/testid0/testid0_collection/1',
-            'value' : 'jimmy'
+            'value' : 'testUpdateMutableDescription'
         }
-        compare_post_request("updateMutableDescription", data, headers = {"Accept": "text/plain"},test_name = "test_edit_mutable")
+        compare_post_request("updateMutableDescription", data, headers = {"Accept": "text/plain"},test_name = "test_edit_mutable_description")
+
+    def test_edit_field(self):
+        data={
+            'uri': 'http://localhost:7777/public/testid0/testid0_collection/1',
+            'value' : 'testUpdateMutableNotes'
+        }
+        compare_post_request("updateMutableNotes", data, headers = {"Accept": "text/plain"},test_name = "test_edit_mutable_notes")
+
+    def test_edit_source(self):
+        data={
+            'uri': 'http://localhost:7777/public/testid0/testid0_collection/1',
+            'value' : 'testUpdateMutableSource'
+        }
+        compare_post_request("updateMutableSource", data, headers = {"Accept": "text/plain"},test_name = "test_edit_mutable_source")
+
+#    def test_edit_citations(self):
+#        data={
+#            'uri': 'http://localhost:7777/public/testid0/testid0_collection/1',
+#           'value' : 'testUpdateCitations'
+#       }
+#        compare_post_request("updateCitations", data, headers = {"Accept": "text/plain"},test_name = "test_edit_mutable_citations")
