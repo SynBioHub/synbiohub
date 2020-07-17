b46443779b2ed53df76dba07da537a4405672a56
jvscholz <jvscholz@gmail.com>
2020-07-17 06:41:47 -0600

    fixes tests

--- tests/test_submit.py
+++ tests/test_submit.py
@@ -132,17 +132,16 @@ class TestSubmit(TestCase):
 #    def test_attach(self):
 #        files={
 #            'file' : open('test_user.py', 'rb')
-#            }
+#        }
 #        data = {
 #        }
 #        compare_post_request("/public/:collectionId/:displayId/:version/attach", data, route_parameters = ["testid0", "testid_collection0", "1"], headers = {"Accept": "text/plain"}, files = files, test_name = "test_attach")
-
-
+#
 #    def test_attachUrl(self):
-        #http://localhost:7777/public/testid0/testid0_collection/1/attachUrl
-#        data = {
-#            'url': 'testsite.com',
+#        #http://localhost:7777/public/testid0/testid0_collection/1/attachUrl
+#        data={
 #            'name' : 'testURLAttachment',
 #            'type' : 'dim'
-#            }
-#        compare_post_request("/public/:collectionId/:displayId/:version/attachUrl", data, route_parameters = ["testid0", "testid_collection0", "1"], headers = {"Accept": "text/plain"}, test_name = "test_attachUrl")
+#        }
+#        compare_post_request("/public/:collectionId/:displayId/:version/attachUrl", data, route_parameters = ["testid0", "testid_collection0", "1"],headers = {"Accept": "text/plain"}, test_name = "test_attachUrl")
+
