1557452852868b1011a45edd8586ab1df61b2702
jvscholz <jvscholz@gmail.com>
2020-08-13 08:38:10 -0600

    initial fixes to test_user and test_edit

--- tests/test_edit.py
+++ tests/test_edit.py
@@ -1,36 +1,46 @@
 import requests
 from unittest import TestCase
+from test_arguments import test_print
 from test_functions import compare_get_request, compare_post_request
 
 class TestEdit(TestCase):
 
-    def test_update_mutableDescription(self):
+    def test_edit(self):
+        # test_update_mutableDescription
+        test_print("test_update_mutableDescription starting")
         data={
             'uri': 'http://localhost:7777/public/testid0/testid0_collection/1',
             'value' : 'testUpdateMutableDescription'
         }
         compare_post_request("updateMutableDescription", data, headers = {"Accept": "text/plain"},test_name = "test_update_mutableDescription")
+        test_print("test_update_mutableDescription completed")
 
-    def test_update_mutableNotes(self):
+        # test_update_mutableNotes
+        test_print("test_update_mutableNotes starting")
         data={
             'uri': 'http://localhost:7777/public/testid0/testid0_collection/1',
             'value' : 'testUpdateMutableNotes'
         }
         compare_post_request("updateMutableNotes", data, headers = {"Accept": "text/plain"},test_name = "test_update_mutableNotes")
+        test_print("test_update_mutableNotes completed")
 
-    def test_update_mutableSource(self):
+        # test_update_mutableSource
+        test_print("test_update_mutableSource starting")
         data={
             'uri': 'http://localhost:7777/public/testid0/testid0_collection/1',
             'value' : 'testUpdateMutableSource'
         }
         compare_post_request("updateMutableSource", data, headers = {"Accept": "text/plain"},test_name = "test_update_mutableSource")
+        test_print("test_update_mutableSource completed")
 
-    def test_edit_citations(self):
+        # test_edit_citations
+        test_print("test_edit_citations starting")
         data={
             'uri': 'http://localhost:7777/public/testid0/testid0_collection/1',
             'value' : '1234'
-       }
+        }
         compare_post_request("updateCitations", data, headers = {"Accept": "text/plain"},test_name = "test_edit_mutable_citations")
+        test_print("test_edit_citations completed")
 
 #    def test_add_field(self):
 #        data={
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,19 +1,11 @@
 from unittest import TestCase
+from test_arguments import test_print
 from test_functions import compare_get_request, compare_post_request, login_with
-
-#/register is tested within the docker_test_setup.py file
 
 class TestUser(TestCase):
 
-#    def test_post_login_token(self):
-#        logininfo = {'email' : 'test@user.synbiohub',
-#                     'password' : 'test'}
-#        login_with(logininfo)
-
-#    def test_logout(self):
-#        compare_get_request("/logout")
-
     def test_post_register(self):
+        test_print("test_post_register starting")
         data={
             'username': 'testuser2',
             'name' : 'ronald',
@@ -41,9 +33,6 @@ class TestUser(TestCase):
 
         compare_get_request("/logout")
 
-#    def test_post_login_token(self):
-#        logininfo = {'email' : 'test@user.synbiohub',
-#                     'password' : 'test'}
-#        login_with(logininfo)
+        test_print("test_post_register completed")
 
 
