101b31c1c63a90ce1bc733cc3c4be19c74b8585e
benjhatch <benjamin.hat4@gmail.com>
2020-09-14 13:26:26 -0600

    changed container to visboldesign to avoid bootstrap css

--- browser/visbol.js
+++ browser/visbol.js
@@ -4,13 +4,13 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 import Rendering from 'visbol-react';
 
-if(document.getElementById('design')
+if(document.getElementById('visboldesign')
     && typeof meta !== 'undefined'
     && meta.displayList) {
 
-    const container = document.getElementById('design');
+    const container = document.getElementById('visboldesign');
     if (typeof window !== 'undefined') {
         const display = prepareDisplay(meta.displayList);
-        ReactDOM.render(<Rendering display={display}/>, container);
+        ReactDOM.render(<Rendering display={display} toLog={meta.displayList}/>, container);
     }
 }
