ca5165ac6a14ea8fa89d93fd04744946f8691f24
benjhatch <benjamin.hat4@gmail.com>
2020-09-21 21:43:59 -0600

    fixed visbol css and rendering too many glyphs based on segmentID

--- browser/visbol.js
+++ browser/visbol.js
@@ -4,11 +4,11 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 import Rendering from 'visbol-react';
 
-if(document.getElementById('visboldesign')
+if(document.getElementById('design')
     && typeof meta !== 'undefined'
     && meta.displayList) {
 
-    const container = document.getElementById('visboldesign');
+    const container = document.getElementById('design');
     if (typeof window !== 'undefined') {
         const display = prepareDisplay(meta.displayList);
         ReactDOM.render(<Rendering display={display} toLog={meta.displayList}/>, container);
