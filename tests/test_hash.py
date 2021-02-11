914438c4aea19907055f77ebd50301d2c1bb6f93
j <jvscholz@gmail.com>
2021-02-11 13:43:42 -0700

    removes root collection

--- tests/test_hash.py
+++ tests/test_hash.py
@@ -9,3 +9,29 @@ class TestHash(TestCase):
 		
 		continue
 
+#user/:userId/:collectionId/:displayId/:version/:hash/share/sbolnr was not tested.
+
+#user/:userId/:collectionId/:displayId/:version/:hash/share/omex was not tested.
+
+#user/:userId/:collectionId/:displayId/:version/:hash/share/summary was not tested.
+#user/:userId/:collectionId/:displayId/:version/:hash/share/fasta was not tested.
+
+#user/:userId/:collectionId/:displayId/:version/:hash/share/gb was not tested.
+
+#user/:userId/:collectionId/:displayId/:version/:hash/share/gff was not tested.
+#user/:userId/:collectionId/:displayId/:version/:hash/share/metadata was not tested.
+
+#user/:userId/:collectionId/:displayId/:version/:hash/share/visualization was not tested.
+
+#user/:userId/:collectionId/:displayId(*)/:version/:hash/share/full was not tested.
+
+#user/:userId/:collectionId/:displayId(*)/:version/:hash/share was not tested.
+
+#user/:userId/:collectionId/:displayId/:version/:hash/share/subCollections was not tested.
+#user/:userId/:collectionId/:displayId/:version/:hash/share/twins was not tested.
+#user/:userId/:collectionId/:displayId/:version/:hash/share/uses was not tested.
+#user/:userId/:collectionId/:displayId/:version/:hash/share/similar was not tested.
+#user/:userId/:collectionId/:displayId/:version/:hash/share/remove was not tested.
+#user/:userId/:collectionId/:displayId/:version/:hash/share/replace was not tested.
+#user/:userId/:collectionId/:displayId/:version/:hash/share/makePublic was not tested.
+
