c9f71ec6ee791851b26b154a74b1e0f47dd9d1e9
j <jvscholz@gmail.com>
2021-03-11 06:12:11 -0700

    fixes test_downlaod

--- tests/test_download.py
+++ tests/test_download.py
@@ -4,33 +4,3 @@ from test_functions import compare_get_request, compare_post_request
 
 class TestDownload(TestCase):
     pass
-#    def test_gff(self):
-        #localhost:7777/public/testid0/BBa_I0462/1/gff
-#        headers = {'Accept': 'text/plain'}
-#        compare_get_request("/public/:collectionId/:displayId/:version/gff", route_parameters =["testid0","BBa_I0462", "1"])
-
-# TEST SBOL 
-    file = open("test.gb").read()
-    diff_file = open("test.gb").read()
-
-    request = { 'options': {'language' : 'GenBank',
-                            'test_equality': True,
-                            'check_uri_compliance': False,
-                            'check_completeness': False,
-                            'check_best_practices': False,
-                            'fail_on_first_error': False,
-                            'provide_detailed_stack_trace': False,
-                            'subset_uri': '',
-                            'uri_prefix': '',
-                            'version': '',
-                            'insert_type': False,
-                            'main_file_name': 'file',
-                            'diff_file_name': 'diff_file',
-                            },
-                'return_file': True,
-                'main_file': file,
-                'diff_file': diff_file
-            }
-
-    resp = requests.post("https://validator.sbolstandard.org/validate/", json=request)
-    print(resp)
