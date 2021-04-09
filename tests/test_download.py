e313acb2bbf1132be81c2cd1ae39f4f0e7eb148f
jvscholz <jvscholz@gmail.com>
2021-04-09 10:48:23 -0600

    rewrites test function to correctly throw errors, rewrite test_get_download to use a different file path that doesn't format to html, tests serveral sbol download endpoints

--- tests/test_download.py
+++ tests/test_download.py
@@ -6,5 +6,13 @@ class TestDownload(TestCase):
 
     def test_download(self):
 
+        compare_get_request_download("/public/:collectionId/:displayId/sbol", route_parameters = ["testid1","part_pIKE_Toggle_1"], headers = {"Accept": "text/html"})
         compare_get_request_download("/public/:collectionId/:displayId/:version/sbol", route_parameters = ["testid1","part_pIKE_Toggle_1","1"], headers = {"Accept": "text/html"})
+        compare_get_request_download("/public/:collectionId/:displayId/sbolnr", route_parameters = ["testid1","part_pIKE_Toggle_1"], headers = {"Accept": "text/html"})
+        compare_get_request_download("/public/:collectionId/:displayId/:version/sbolnr", route_parameters = ["testid1","part_pIKE_Toggle_1","1"], headers = {"Accept": "text/html"})
 
+        # user/testuser/test_attachment/part_pIKE_Toggle_1/1
+        compare_get_request_download("/user/:userId/:collectionId/:displayId/sbol", route_parameters = ["testuser","test_attachment","part_pIKE_Toggle_1"], headers = {"Accept": "text/html"})
+        compare_get_request_download("/user/:userId/:collectionId/:displayId/sbolnr", route_parameters = ["testuser","test_attachment","part_pIKE_Toggle_1"], headers = {"Accept": "text/html"})
+        compare_get_request_download("/user/:userId/:collectionId/:displayId/:version/sbol", route_parameters = ["testuser","test_attachment","part_pIKE_Toggle_1","1"], headers = {"Accept": "text/html"})
+        compare_get_request_download("/user/:userId/:collectionId/:displayId/:version/sbolnr", route_parameters = ["testuser","test_attachment","part_pIKE_Toggle_1","1"], headers = {"Accept": "text/html"})
