e61915c003e09d168f2d5d4222031fcdefceb893
j <jvscholz@gmail.com>
2021-04-12 15:43:53 -0600

    tests more download endpoints

--- tests/test_download.py
+++ tests/test_download.py
@@ -5,7 +5,6 @@ from test_functions import compare_get_request, compare_get_request_download,com
 class TestDownload(TestCase):
 
     def test_download(self):
-
         compare_get_request_download("/public/:collectionId/:displayId/sbol", route_parameters = ["testid1","part_pIKE_Toggle_1"], headers = {"Accept": "text/html"})
         compare_get_request_download("/public/:collectionId/:displayId/:version/sbol", route_parameters = ["testid1","part_pIKE_Toggle_1","1"], headers = {"Accept": "text/html"})
         compare_get_request_download("/public/:collectionId/:displayId/sbolnr", route_parameters = ["testid1","part_pIKE_Toggle_1"], headers = {"Accept": "text/html"})
@@ -16,3 +15,15 @@ class TestDownload(TestCase):
         compare_get_request_download("/user/:userId/:collectionId/:displayId/sbolnr", route_parameters = ["testuser","test_attachment","part_pIKE_Toggle_1"], headers = {"Accept": "text/html"})
         compare_get_request_download("/user/:userId/:collectionId/:displayId/:version/sbol", route_parameters = ["testuser","test_attachment","part_pIKE_Toggle_1","1"], headers = {"Accept": "text/html"})
         compare_get_request_download("/user/:userId/:collectionId/:displayId/:version/sbolnr", route_parameters = ["testuser","test_attachment","part_pIKE_Toggle_1","1"], headers = {"Accept": "text/html"})
+
+        compare_get_request_download("/public/:collectionId/:displayId/:version/gff", route_parameters = ["testid1","part_pIKE_Toggle_1","1"], headers = {"Accept": "text/html"})
+        compare_get_request_download("/public/:collectionId/:displayId/:version/omex", route_parameters = ["testid1","part_pIKE_Toggle_1","1"], headers = {"Accept": "text/html"})
+        compare_get_request_download("/public/:collectionId/:displayId/:version/fasta", route_parameters = ["testid1","part_pIKE_Toggle_1","1"], headers = {"Accept": "text/html"})
+        # compare_get_request_download("/public/:collectionId/:displayId/summary", route_parameters = ["testid1","part_pIKE_Toggle_1"], headers = {"Accept": "text/html"})
+
+        compare_get_request_download("/user/:userId/:collectionId/:displayId/:version/gff", route_parameters = ["testuser","test_attachment","part_pIKE_Toggle_1","1"], headers = {"Accept": "text/html"})
+        compare_get_request_download("/user/:userId/:collectionId/:displayId/:version/omex", route_parameters = ["testuser","test_attachment","part_pIKE_Toggle_1","1"], headers = {"Accept": "text/html"})
+        compare_get_request_download("/user/:userId/:collectionId/:displayId/:version/fasta", route_parameters = ["testuser","test_attachment","part_pIKE_Toggle_1","1"], headers = {"Accept": "text/html"})
+
+        # http://localhost:7777/user/testuser/test_hash/attachment_0ff5d71561d144b7b367765a8c1f2d00/1/download
+        # compare_get_request_download("/user/:userId/:collectionId/:displayId/:version/download", route_parameters = ["testuser","test_hash","attachment_0ff5d71561d144b7b367765a8c1f2d00","1"], headers = {"Accept": "text/html"})
