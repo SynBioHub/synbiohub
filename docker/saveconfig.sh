dabe66aac33e410db9e361815153cea136146605
Peter Hindes <19994487+PeterHindes@users.noreply.github.com>
2025-08-13 11:51:24 -0600

    simplify changes

--- docker/saveconfig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-# Save git revision to config before removing .git folder
-echo "Saving git revision to configuration..."
-node gitrevupdate.js
-
-echo "Git revision saved successfully!"
